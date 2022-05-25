73603017fc1f6d05d8a805d080530d14a405c480
keita-abe1022 <keita.abe1022@gmail.com>
2022-05-25 17:10:16 +0000

    新規登録ページ仮組み

--- app/javascript/pages/Signup.vue
+++ app/javascript/pages/Signup.vue
@@ -35,7 +35,52 @@
 
 <script>
 export default {
-
+  name: 'Signup',
+  data() {
+    return {
+      user: {
+        user_name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+        error: ''
+      }
+    }
+  },
+  created() {
+    this.checkSignedIn()
+  },
+  updated() {
+    this.checkSignedIn()
+  },
+  methods: {
+    signUp() {
+      this.$http.plain.post('/api/signup', { user_name: this.user_name, email: this.email, password: this.password, password_confirmation: this.password_confirmation })
+        .then(response => this.signupSuccessful(response))
+        .catch(error => this.signupFailed(error))
+    },
+    signupSuccessful(response) {
+      if (!response.data.csrf) {
+        this.signupFailed(response)
+        return
+      }
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+      this.$store.dispatch('doFetchSignedIn')
+      this.error= ''
+      this.$roter.replace('/')
+    },
+    signupFailed(error) {
+      this.error = (error.response && error.response.data && error.response.data.error) || 'Something went wrong'
+      delete localStorage.csrf
+      delete localStorage.signedIn
+    },
+    checkSignedIn() {
+      if (localStorage.signedIn) {
+        this.$router.replace('/')
+      }
+    }
+  }
 }
 </script>
 
